1e77fa4ff7e7a3f7881e1f43a8d32461632d3f0d
Rakesh Roshan M N <rakeshroshan@gmail.com>
2025-05-24 14:28:54 +1000

    Updated data and added few Properties

--- js/main.js
+++ js/main.js
@@ -1,32 +1,75 @@
 document.addEventListener("DOMContentLoaded", () => {
   const properties = [
     {
-      title: "DTCP Approved Plot in Malayankulam",
-      location: "Malayankulam, Tamil Nadu",
-      price: "Rs.40 Lakhs per Acker, Negotiable",
-      status: "Available",
-      description:
-        "Premium DTCP approved plots in Malayankulam surrounded by green landscapes.",
+      title: "130 Acres of Prime Land for Sale on GST Road, Melmarvathur",
+      location: "GST Road, before Melmarvathur (right-hand side)",
+      Land_Type: "130 acres total - 100 acres dry land and 30 acres wetland",
+      Ownership: "Single owner",
+      Features: "Fully enclosed with compound wall",
+      price: "Rs.35 lakhs per acre",
       images: [
-        "./images/sample-property-1(1).jpg",
-        "./images/sample-property-1(2).jpg",
-        "./images/sample-property-1(3).jpg",
-        "./images/sample-property-1(4).jpg",
-        "./images/sample-property-1(5).jpg",
-        "./images/sample-property-1(6).jpg",
-        "./images/sample-property-1(7).jpg",
-        "./images/sample-property-1(8).jpg",
-        "./images/sample-property-1(9).jpg",
-        "./images/sample-property-1(10).jpg",
-        "./images/sample-property-1(11).jpg",
-        "./images/sketch-property-1.png",
+        "./images/property-1/melmaruvathur-property.png",
+        "./images/property-1/mel-plot-img-1.jpg",
+        "./images/property-1/mel-plot-img-2.jpg",
+        "./images/property-1/mel-plot-img-3.jpg",
       ],
-      videos: [
-        "./videos/sample-property-1(12).mp4",
-        "./videos/sample-property-1(13).mp4",
-      ],
+      videos: [],
       mapEmbedUrl:
-        "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3892.3239453281194!2d79.807654!3d12.692267000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDQxJzMyLjIiTiA3OcKwNDgnMjcuNiJF!5e0!3m2!1sen!2sau!4v1747872630437!5m2!1sen!2sau",
+        "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3895.5491239165194!2d79.78296499999999!3d12.479719000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDI4JzQ3LjAiTiA3OcKwNDYnNTguNyJF!5e0!3m2!1sen!2sau!4v1748057039482!5m2!1sen!2sau",
+    },
+
+    {
+      title: "Prime GST Road Land - 100+ Acres for Sale",
+      location: "GST Road, stretch between Chennai and Maduranthakam",
+      Land_Type: "100 to 120 acres - combination of wet and dry land",
+      Ownership: "3 partners",
+      Features: "Suitable for large-scale development or investment",
+      price: "Rs.35 lakhs per acre (negotiable)",
+      images: [
+        "./images/property-2/Maduranthakam-property.png",
+        "./images/property-2/mad-plot-img-1.jpg",
+        "./images/property-2/mad-plot-img-2.jpg",
+      ],
+      videos: ["./videos/mad-plot-vid-1.mp4"],
+      mapEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3894.8969850461053!2d79.841881!3d12.522983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDMxJzIyLjciTiA3OcKwNTAnMzAuOCJF!5e0!3m2!1sen!2sau!4v1748058481786!5m2!1sen!2sau",
+    },
+
+    {
+      title: "Land for Sale - Mannampoondi & Puliyanur, Tindivanam",
+      location:
+        "Mannampoondi and Puliyanur Village, Tindivanam Taluk, Villupuram District, Tamil Nadu",
+      Land_Type: "104 acres of dry land",
+      Ownership: "Single owner",
+      Features: "Ideal for agriculture or investment",
+      price: "Rs.35 lakhs per acre",
+      images: [
+        "./images/property-3/Mannampoondi-property.png",
+        "./images/property-3/man-plot-img-1.jpg",
+        "./images/property-3/man-plot-img-2.jpg",
+      ],
+      videos: ["./videos/man-plot-vid-1.mp4"],
+      mapEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3898.3009766083856!2d79.56160677506509!3d12.295506687961183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDE3JzQzLjgiTiA3OcKwMzMnNTEuMSJF!5e0!3m2!1sen!2sau!4v1748059416034!5m2!1sen!2sau",
+    },
+
+    {
+      title: "Land for Sale - Elapakkam & Rettamangalam, Kancheepuram",
+      location:
+        "Elapakkam (V.No. 70) & Rettamangalam (V.No. 71), Uthiramerur Taluk, Kancheepuram District, Tamil Nadu",
+      Land_Type: "135 acres of dry land",
+      Ownership: "Single owner",
+      Features:
+        "Large contiguous area, suitable for agriculture or development",
+      price: "Rs.40 lakhs per acre (negotiable)",
+      images: [
+        "./images/property-4/Elapakkam-property.png",
+        "./images/property-4/Elapakkam-kootu.png",
+        "./images/property-4/ela-plot-img-1.jpg",
+      ],
+      videos: [],
+      mapEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3892.295531324342!2d79.80082999999999!3d12.694123999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTLCsDQxJzM4LjkiTiA3OcKwNDgnMDMuMCJF!5e0!3m2!1sen!2sau!4v1748060055225!5m2!1sen!2sau",
     },
   ];
 
@@ -109,9 +152,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const details = document.createElement("div");
     details.innerHTML = `
       <p><strong>Location:</strong> ${property.location}</p>
+      <p><strong>Land Type:</strong> ${property.Land_Type}</p>
+      <p><strong>Ownership:</strong> ${property.Ownership}</p>
+      <p><strong>Features:</strong> ${property.Features}</p>
       <p><strong>Price:</strong> ${property.price}</p>
-      <p><strong>Status:</strong> ${property.status}</p>
-      <p>${property.description}</p>
     `;
 
     const map = document.createElement("iframe");
